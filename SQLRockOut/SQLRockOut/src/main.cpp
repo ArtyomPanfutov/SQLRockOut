d7c6deab3f2f5b4f7b543f9a68696556e35a3eb0
Артем <artyom.cox@gmail.com>
2019-02-23 00:15:01 +0300

    added xcode project

---
+++ SQLRockOut/SQLRockOut/src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SQLRockOut
+//
+//  Created by Артем on 23/02/2019.
+//  Copyright © 2019 Artyom Panfutov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
