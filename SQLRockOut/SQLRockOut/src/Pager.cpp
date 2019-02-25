fa15acafa4fb9cb8407c3b4fdbc414777b55e113
Артем <artyom.cox@gmail.com>
2019-02-25 22:23:22 +0300

    implemented page fetching

--- SQLRockOut/SQLRockOut/src/Pager.cpp
+++ SQLRockOut/SQLRockOut/src/Pager.cpp
@@ -35,3 +35,38 @@ Pager* pager_open(const char* filename)
     
     return pager;
 }
+
+void* get_page(Pager* pager, uint32_t page_num)
+{
+    if (page_num > TABLE_MAX_ROWS)
+    {
+        printf("Fetching page out of bounds. %d", TABLE_MAX_ROWS);
+        exit(EXIT_FAILURE);
+    }
+    
+    if (pager->pages[page_num] == NULL)
+    {
+        // Cache miss. Allocate memory and load from file.
+        void* page = malloc(PAGE_SIZE);
+        uint32_t num_pages = pager->file_length / PAGE_SIZE;
+        
+        // We might save a partial page at the end of the file
+        if (pager->file_length % PAGE_SIZE)
+        {
+            num_pages += 1;
+        }
+        
+        if (page_num <= num_pages)
+        {
+            lseek(pager->file_descriptor, page_num * PAGE_SIZE, SEEK_SET);
+            ssize_t bytes_read = read(pager->file_descriptor, page, PAGE_SIZE);
+            if (bytes_read == -1)
+            {
+                printf("Error reading file: %d\n", errno);
+                exit(EXIT_FAILURE);
+            }
+        }
+        pager->pages[page_num] = page;
+    }
+    return pager->pages[page_num];
+}
